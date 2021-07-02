a5e061839b45f7db788b43a2e727a4f23fef44dd
Boikissimo <boiko.bruno@gmail.com>
2021-07-02 14:48:51 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sabores
+{
+    class Program
+    {
+        static listaSabores sabores
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+        static void add(string sabor, string tipo, string int) {
+            
+        }
+
+    }
+}
